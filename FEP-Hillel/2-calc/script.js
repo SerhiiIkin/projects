e6b03451e4d1d517d32ac635ecb385c3d8ec7a35
SerhiiIkin <messileonl@gmail.com>
2021-12-16 22:48:55 +0100

    litle fix

--- FEP-Hillel/2-calc/script.js
+++ FEP-Hillel/2-calc/script.js
@@ -1,34 +1,59 @@
 function math() {
-    const mathAction = prompt("What you want to do ? (+ - / *");
-    const firstNumber = +prompt("Writte first number:");
-    const secondNumber = +prompt("Writte second number:");
-    result(mathAction, firstNumber, secondNumber);
+    const mathAction = getAction();
+    const firstNumber = getNumber("1");
+    const secondNumber = getNumber("2");
+    const result = calc(mathAction, firstNumber, secondNumber);
+
+    showResult(mathAction, firstNumber, secondNumber, result);
+
+    function showResult(mathAction, firstNumber, secondNumber, result) {
+        console.log(`${firstNumber} ${mathAction} ${secondNumber} = ${result}`);
+    }
+
+    function getNumber(num) {
+        const input = +prompt(`Writte number #${num}`);
+        if (isNaN(input)) {
+            console.error("You can writte only numbers!")
+        }
+        return input;
+    }
+
+
+    function calc(mathAction, firstNumber, secondNumber, result) {
+        if (mathAction === "+") {
+            return add(firstNumber, secondNumber);
+        } else if (mathAction === "/") {
+            return div(firstNumber, secondNumber);
+        } else if (mathAction === "*") {
+            return sub(firstNumber, secondNumber);
+        } else if (mathAction === "-") {
+            return mul(firstNumber, secondNumber);
+        } else {
+            return "You need to write right action!";
+        }
+    }
+
+    function add(firstNumber, secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    function mul(firstNumber, secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    function sub(firstNumber, secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    function div(firstNumber, secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    function getAction() {
+        return prompt("What you want to do ? (+ - / *");
+    }
+
 }
 
-const result = function (mathAction, firstNumber, secondNumber) {
-    if (isNaN(firstNumber)) {
-        console.error("Wrong enter first number!");
-        return;
-    }
-    if (isNaN(secondNumber)) {
-        console.error("Wrong enter second number!");
-        return;
-    }
-    if (mathAction === "+") {
-        console.log(`${firstNumber} ${mathAction} ${secondNumber} = ${firstNumber + secondNumber}`);
-        return;
-    } else if (mathAction === "/") {
-        console.log(`${firstNumber} ${mathAction} ${secondNumber} = ${firstNumber / secondNumber}`);
-        return;
-    } else if (mathAction === "*") {
-        console.log(`${firstNumber} ${mathAction} ${secondNumber} = ${firstNumber * secondNumber}`);
-        return;
-    } else if (mathAction === "-") {
-        console.log(`${firstNumber} ${mathAction} ${secondNumber} = ${firstNumber - secondNumber}`);
-        return;
-    } else {
-        console.error("You need to write right action!");
-    }
-    return;
-}
+
 math();
