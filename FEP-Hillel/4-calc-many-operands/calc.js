67413cf3fb22797527ee5958f486f35142bf8b84
SerhiiIkin <messileonl@gmail.com>
2021-12-27 17:10:27 +0100

    home work 4

--- FEP-Hillel/4-calc-many-operands/calc.js
+++ FEP-Hillel/4-calc-many-operands/calc.js
@@ -1,26 +1,20 @@
 function math() {
     const ACTION = {
-        "+": allAction,
-        "-": allAction,
-        "/": allAction,
-        "*": allAction
-    };
-    const ACTION_SHOW = {
-        2: showConsole,
-        3: showConsole,
-        4: showConsole,
-        5: showConsole
+        "+": add,
+        "-": mul,
+        "/": div,
+        "*": sub
     };
     const MIN_NUMBER_COUNT = 1;
     const MAX_NUMBER_COUNT = 5;
     const ACTION_LIST = Object.keys(ACTION);
 
     const mathAction = getAction();
-    const count = getNumberLong();
-    const number = getNumber(count);
+    const count = getLengthNumber();
+    const number = getNumbers(count);
     const result = calc(mathAction, number);
 
-    showResult(mathAction, number, result, count);
+    showResult(mathAction, number, result);
 
     function getAction() {
         let input;
@@ -29,36 +23,36 @@ function math() {
             input = prompt(`What you want to do ? ${ACTION_LIST.join(" ")}`);
         } while (!actionError(input));
 
-        function actionError(input) {
-            if (!ACTION_LIST.includes(input)) {
-                alert("Writte correct symbol!");
-            } else if (ACTION_LIST.includes(input)) {
-                return true;
-            }
-        }
-
         return input;
     }
 
-    function getNumberLong() {
+    function actionError(input) {
+        if (!ACTION_LIST.includes(input)) {
+            alert("Writte correct symbol!");
+        } else if (ACTION_LIST.includes(input)) {
+            return true;
+        }
+    }
+
+    function getLengthNumber() {
         let count;
 
         do {
             count = prompt(`How many digits, do you want to write? It is possible to enter only numbers from 2 to 5`);
-        } while (!numberLongError(count));
-
-        function numberLongError(count) {
-            if (!(count > MIN_NUMBER_COUNT && count <= MAX_NUMBER_COUNT)) {
-                alert("You can writte numbers only from 2 to 5! ");
-            } else {
-                return true;
-            }
-        }
+        } while (!checkLengthNumber(count));
 
         return +count;
     }
 
-    function getNumber(count) {
+    function checkLengthNumber(count) {
+        if (!(count > MIN_NUMBER_COUNT && count <= MAX_NUMBER_COUNT)) {
+            alert("You can writte numbers only from 2 to 5! ");
+        } else {
+            return true;
+        }
+    }
+
+    function getNumbers(count) {
         let inputNumbers = [];
         let i, j, element;
 
@@ -67,47 +61,41 @@ function math() {
 
                 inputNumbers[i] = prompt(`Writte number #${j}`);
                 element = inputNumbers[i];
-            } while (!numberError(element));
-        }
 
-        function numberError(element) {
-            if (isNaN(element) || !element || element === " ") {
-                alert("You can write only numbers!");
-            } else {
-                return true;
-            }
+            } while (!numbersError(element));
         }
 
         return inputNumbers.map(Number);
     }
 
+    function numbersError(element) {
+        if (isNaN(element) || !element || element === " ") {
+            alert("You can write only numbers!");
+        } else {
+            return true;
+        }
+    }
+
     function calc(mathAction, number) {
-        return ACTION[mathAction](number);
+        const mathFunc = ACTION[mathAction]
+        return number.reduce((acc, current) => mathFunc(acc, current));
     }
 
-    function allAction(number) {
-        let result = number.reduce(function (action, current) {
-            switch (mathAction) {
-                case "+":
-                    return action + current;
-                case "-":
-                    return action - current;
-                case "*":
-                    return action * current;
-                case "/":
-                    return action / current;
-            }
-        });
-
-        return result;
+    function add(acc, current) {
+        return acc + current;
+    }
+    function sub(acc, current) {
+        return acc * current;
+    }
+    function mul(acc, current) {
+        return acc - current;
+    }
+    function div(acc, current) {
+        return acc / current;
     }
 
-    function showResult(mathAction, number, result, count) {
-        return ACTION_SHOW[count](number);
-    }
-
-    function showConsole(number) {
-        console.log(`${number.join(" " + mathAction + " ")} = ${result}`);
+    function showResult(mathAction, number, result) {
+        console.log( `${number.join(" " + mathAction + " ")} = ${result}` );
     }
 
 }
