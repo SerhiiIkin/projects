517029bfba07bd1e270fa04e54045997113e074c
SerhiiIkin <messileonl@gmail.com>
2021-12-28 01:05:07 +0100

    home work 5

--- FEP-Hillel/5-students/students.js
+++ FEP-Hillel/5-students/students.js
@@ -2,45 +2,47 @@ const students = [
     {
         id: 10,
         name: "John Smith",
-        marks: [ 10, 8, 6, 9, 8, 7]
+        marks: [10, 8, 6, 9, 8, 7],
     },
     {
         id: 11,
         name: "John Doe",
-        marks: [ 9, 8, 7, 6, 7]
+        marks: [9, 8, 7, 6, 7],
     },
     {
         id: 12,
         name: "Thomas Anderson",
-        marks: [6, 7, 10, 8 ]
+        marks: [6, 7, 10, 8],
     },
     {
         id: 13,
         name: "Jean-Baptiste Emanuel Zorg",
-        marks: [10, 9, 8, 9 ]
-    }
-]
+        marks: [10, 9, 8, 9],
+    },
+];
 
-console.log(avarageStudentMark(10)); 
-console.log(averageGroupMark(students));
-
+console.log("Средний бал группы из id:10 =" + " " + avarageStudentMark(10));
+console.log("Средний бал группы из id:11 =" + " " + avarageStudentMark(11));
+console.log("Средний бал группы из id:12 =" + " " + avarageStudentMark(12));
+console.log("Средний бал группы из id:13 =" + " " + avarageStudentMark(13));
+console.log("Общий средний бал группы =" + " " +averageGroupMark(students));
 
 function avarageStudentMark(id) {
-
-    let getIdMas = students.find(item => item.id === id);
+    let getIdMas = students.find((item) => item.id === id);
 
     return getIdMas ? calcArithmeticMean(getIdMas.marks) : null;
 }
 
 function averageGroupMark(students) {
-    let allMarks = [];
+    let allMarksStudentAvarage = [];
 
     for (let student of students) {
-        allMarks.push(calcArithmeticMean(student.marks));
+        allMarksStudentAvarage.push(calcArithmeticMean(student.marks));
     }
-    return calcArithmeticMean(allMarks);
+
+    return calcArithmeticMean(allMarksStudentAvarage);
 }
 
 function calcArithmeticMean(item) {
-    return item.reduce((sum, avar) => sum + avar / item.length, 0)
+    return item.reduce((sum, avar) => sum + avar / item.length, 0);
 }
