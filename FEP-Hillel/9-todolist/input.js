dc8f6d2a26cc3817043dda380eb787eb514f4a9a
SerhiiIkin <messileonl@gmail.com>
2022-01-11 16:28:13 +0100

    home work 9

--- FEP-Hillel/9-todolist/input.js
+++ FEP-Hillel/9-todolist/input.js
@@ -1,24 +1,24 @@
-const myList = document.querySelector(".list");
-const inputText = document.querySelector(".input");
+const listEl = document.querySelector(".list");
+const inputEl = document.querySelector(".input");
 const addBtn = document.querySelector(".btn");
 let errorDiv = document.createElement("div");
 
 addBtn.addEventListener("click", onClickBtn);
 
 function onClickBtn() {
-    let nyLi = document.createElement("li");
-    let value = inputText.value;
+    let newLiEl = document.createElement("li");
+    let value = inputEl.value;
 
-    if (value !== "") {
-        nyLi.textContent = value;
-        myList.append(nyLi);
+    if (value === "") {
+        errorDiv.textContent = "Enter text in input!";
+        errorDiv.classList.add("red", "nyDiv");
+        errorDiv.classList.remove("hide");
+        inputEl.after(errorDiv);
+    } else {
+        newLiEl.textContent = value;
+        listEl.append(newLiEl);
         value = null;
         errorDiv.classList.remove("red");
         errorDiv.classList.add("hide", "nyDiv");
-    } else {
-        errorDiv.innerHTML = "Enter text in input!";
-        errorDiv.classList.add("red", "nyDiv");
-        errorDiv.classList.remove("hide");
-        inputText.after(errorDiv);
     }
 }
