abe6a0519188a7417242d55b009f10f57bfdaaec
SerhiiIkin <messileonl@gmail.com>
2022-01-27 23:25:39 +0100

    home work 14

--- FEP-Hillel/14-group-of-students/index.js
+++ FEP-Hillel/14-group-of-students/index.js
@@ -10,8 +10,8 @@ group.addStudent = new Student("Bob", [6, 10]);
 console.log(group.students.length === 3);
 group.addStudent = {};
 console.log(group.students.length === 3);
+
 console.log(group.averageMark);
 
-console.log(group.students);
 group.students = new Student("John", [10, 10, 5, 10]);
 console.log(group.students.length === 3);
