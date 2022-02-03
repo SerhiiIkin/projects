b50efc97548ff39316cc80b5dc98f3de0b026f45
SerhiiIkin <messileonl@gmail.com>
2022-02-03 21:00:06 +0100

    home work 15

--- FEP-Hillel/15-tabs/tabs.js
+++ FEP-Hillel/15-tabs/tabs.js
@@ -8,67 +8,53 @@ class Tabs {
 
     constructor(tabs) {
         this.#tabs = tabs;
+        this.buttons = this.#tabs.children[0].children;
+        this.contents = this.#tabs.children[1].children;
+        this.tabs = this.#tabs.children[0];
 
         this.tabsAddClass();
         this.tabsEvents();
     }
 
     tabsEvents() {
-        this.#tabs.addEventListener("click", (e) => this.onTabsClick(e));
+        this.tabs.addEventListener("click", (e) => this.onTabsClick(e));
     }
 
     tabsAddClass() {
-        const containers = this.#tabs.children;
-        const [buttonContainer, contentContainer] = containers;
-        const buttons = buttonContainer.children;
-
-        for (let container of containers) {
-            container.classList.add("tabs__container");
-        }
-
-        for (let i = 0; i < buttons.length; i++) {
-            const button = buttons[i];
-            const content = contentContainer.children[i];
+        for (let i = 0; i < this.buttons.length; i++) {
+            const button = this.buttons[i];
+            const content = this.contents[i];
             button.classList.add(Tabs.CLASS_BUTTON);
             content.classList.add(Tabs.CLASS_ITEMS);
-            button.classList.add(Tabs.CLASS_BUTTON + "-" + (i + 1));
-            content.classList.add(Tabs.CLASS_ITEMS + "-" + (i + 1));
         }
 
-        const firstTab = contentContainer.querySelector(
-            "." + Tabs.CLASS_ITEMS + "-1"
-        );
-        const firstBtn = buttons[0];
+        const firstTab = this.contents[0];
+        const firstBtn = this.buttons[0];
 
         this.toggleClass(firstTab, Tabs.CLASS_OPEN);
         this.toggleClass(firstBtn, Tabs.CLASS_CHANGE);
     }
 
     onTabsClick(e) {
-        const buttonOne = this.containsClassButton(e.target, 1);
-        const buttonTwo = this.containsClassButton(e.target, 2);
-        const buttonTree = this.containsClassButton(e.target, 3);
         const allTabsContent = this.#tabs.querySelectorAll(
             "." + Tabs.CLASS_ITEMS
         );
         const allBtn = this.#tabs.querySelectorAll("." + Tabs.CLASS_BUTTON);
 
-        if (buttonOne) {
+        if (e.target.closest("." + Tabs.CLASS_BUTTON)) {
+            const index = this.findBtn(e.target, allBtn);
             this.removeClass(allTabsContent, allBtn);
-            this.toggleClass(allTabsContent[0], Tabs.CLASS_OPEN);
-            this.toggleClass(allBtn[0], Tabs.CLASS_CHANGE);
+            this.toggleClass(allTabsContent[index], Tabs.CLASS_OPEN);
+            this.toggleClass(allBtn[index], Tabs.CLASS_CHANGE);
         }
+    }
 
-        if (buttonTwo) {
-            this.removeClass(allTabsContent, allBtn);
-            this.toggleClass(allTabsContent[1], Tabs.CLASS_OPEN);
-            this.toggleClass(allBtn[1], Tabs.CLASS_CHANGE);
-        }
-
-        if (buttonTree) {
-            this.removeClass(allTabsContent, allBtn);
-            this.toggleClass(allTabsContent[2], Tabs.CLASS_OPEN);
-            this.toggleClass(allBtn[2], Tabs.CLASS_CHANGE);
+    findBtn(target, allBtn) {
+        for (let i = 0; i < allBtn.length; i++) {
+            const btn = allBtn[i];
+            if (btn === target) {
+                return i;
+            }
         }
     }
 
@@ -80,7 +66,7 @@ class Tabs {
             if (btn.classList.contains(Tabs.CLASS_CHANGE)) {
                 this.toggleClass(btn, Tabs.CLASS_CHANGE);
             }
-            
+
             if (tab.classList.contains(Tabs.CLASS_OPEN)) {
                 this.toggleClass(tab, Tabs.CLASS_OPEN);
             }
@@ -89,10 +75,6 @@ class Tabs {
 
     toggleClass(el, elClass) {
         el.classList.toggle(elClass);
-    }
-
-    containsClassButton(target, num) {
-        return target.classList.contains(Tabs.CLASS_BUTTON + "-" + num);
     }
 }
 
