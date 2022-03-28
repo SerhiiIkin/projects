72caa926799472766b2557123461331e8497112a
SerhiiIkin <messileonl@gmail.com>
2022-03-28 21:06:16 +0200

    home work 23

---
+++ FEP-Hillel/23-nodejs/calculator.js
@@ -0,0 +1,6 @@
+const { add } = require("./functions/add");
+const { div } = require("./functions/div");
+const { mult } = require("./functions/mult");
+const { sub } = require("./functions/sub");
+
+module.exports = { add, div, mult, sub };
