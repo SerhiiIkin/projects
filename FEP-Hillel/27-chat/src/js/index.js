1fa297660c154484415c9061fa26e3ecfc4bccca
SerhiiIkin <messileonl@gmail.com>
2022-04-12 22:37:01 +0200

    home work 27

--- FEP-Hillel/27-chat/src/js/index.js
+++ FEP-Hillel/27-chat/src/js/index.js
@@ -4,3 +4,12 @@ import Controller from "./Controller/Controller.js";
 import "../style.css";
 
 new Controller($("#container"));
+
+let regexp = /love/;
+console.log(regexp.test("I love JavaScript"));
+console.log(regexp.test("I JavaScript"));
+
+let regexp2 = /[ing]$/;
+
+console.log(regexp2.test("Good morning"));
+console.log(regexp2.test("Good morning!"));
